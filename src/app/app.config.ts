8f8aa0172c8be1ec5d2daaed51363e8407f8d1f0
yo-huaca95 <yo.huaca@udla.edu.co>
2026-08-10 21:54:55 -0500

    configuración para de cambio de idiomas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import localeEs from '@angular/common/locales/es-CO';
+import localeEUS from '@angular/common/locales/en-ES';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs,'es');
+registerLocaleData(localeEUS,'us');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide:LOCALE_ID,
+      useValue:'us'
+    }
   ]
 };

7ad065accd380a4bb6cb8d2b62b8e1265f43e868
yo-huaca95 <yo.huaca@udla.edu.co>
2026-08-28 14:21:30 -0500

    Implementando I18nPluralPipe, para cambios visuales en el texto segun el número de clientes en espera

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1={
   name:'Yoan',
@@ -19,10 +19,12 @@ const client2={
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card,I18nSelectPipe],
+  imports: [Card,I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {
+  //I18nSelectPipe 
+
   client =signal(client1);
 
   invitationMap={
@@ -37,4 +39,30 @@ export default class UncommonPage {
     }
     this.client.set(client1);
   }
+
+  //I18nPluralPipe
+
+  clientsMap=signal({ 
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando.',
+    'other':'tenemos # clientes esperando.'
+  });
+
+  clients =signal([
+    'Yoan',
+    'Camila',
+    'Julián',
+    'Julieth',
+    'Johana',
+    'Juliana',
+    'Camilo',
+    'Carlos',
+    'Jorge',
+    'Juan'
+  ]);
+
+  deleteClient(){
+    this.clients.update((prev)=> prev.slice(1));
+  }
+
 }

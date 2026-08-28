87d989cd6df39039f3107176810d8f0fbb285fd9
yo-huaca95 <yo.huaca@udla.edu.co>
2026-08-28 16:31:52 -0500

    Implementando SlicePipe, para cambios visuales en el texto obtener datos que se requieran de una lista

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
-import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
 
 const client1={
   name:'Yoan',
@@ -19,7 +19,7 @@ const client2={
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card,I18nSelectPipe, I18nPluralPipe],
+  imports: [Card,I18nSelectPipe, I18nPluralPipe, SlicePipe],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {

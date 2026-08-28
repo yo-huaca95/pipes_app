bc33cb268f04e325b07d8bd4da58f4bf594701a3
yo-huaca95 <yo.huaca@udla.edu.co>
2026-08-28 09:54:21 -0500

    Implementando I18nSelectPipe, para cambios visuales en el texto segun el genero, (invitarlo -> invitarla)

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1={
+  name:'Yoan',
+  gender:'male',
+  age:31,
+  address:'Florencia, Canadá'
+}
+
+const client2={
+  name:'Camila',
+  gender:'female',
+  age:33,
+  address:'Florencia, Canadá'
+}
+
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card,I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+  client =signal(client1);
+
+  invitationMap={
+    male:'invitarlo',
+    female:'invitarla'
+  };
+
+  changeClient(){
+    if(this.client()===client1){
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+}
